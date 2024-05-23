ff86b54cb6d4f000e4efe13716363d471daa0dc8
titien orefice <titien.orefice@gmail.com>
2024-05-23 16:23:53 +0200

    bookmark

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,34 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to lists_path(@list)
+    else
+      @lists = List.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+
+  before_destroy :ensure_not_referenced_by_any_bookmark
+
+  private
+
+  def ensure_not_referenced_by_any_bookmark
+    unless bookmarks.empty?
+      errors.add(:base, 'Cannot delete movie while bookmarks reference it')
+      throw(:abort)
+    end
+  end
 end
